d995f574abbd526515f4329b082989320e2e65cf
David Pärsson <david@parsson.se>
2019-10-05 15:43:08 +0200

    Extend possible entrypoints to include npm, node and gulp

--- nvm-lazy.plugin.zsh
+++ nvm-lazy.plugin.zsh
@@ -1,6 +1,13 @@
-# Lazy load nvm
-function nvm() {
-    unset -f nvm
-    zgen oh-my-zsh plugins/nvm
-    nvm $@
-}
+# Lazy load nvm, with multiple entrypoints
+ZSH_LAZY_NVM_BINARIES=('nvm' 'npm' 'node' 'gulp')
+for NODE_BINARY in $ZSH_LAZY_NVM_BINARIES
+do
+    $NODE_BINARY() {
+        for FUNCTION in $ZSH_LAZY_NVM_BINARIES
+        do
+            unset -f $FUNCTION
+        done
+        zgen oh-my-zsh plugins/nvm
+        $NODE_BINARY $@
+    }
+done
