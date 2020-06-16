62c7f4882e0bad1e1c3ee7168d682e8e3952e12a
David Pärsson <david@parsson.se>
2020-06-16 11:22:34 +0200

    Forward calls to the proper binary
    Not just the last declared one.

--- nvm-lazy.plugin.zsh
+++ nvm-lazy.plugin.zsh
@@ -4,14 +4,17 @@ if [ -z "$ZSH_LAZY_NVM_BINARIES" ]; then
     ZSH_LAZY_NVM_BINARIES=('nvm' 'npm' 'node')
 fi
 
+_zsh_lazy_nvm_replace_binaries_and_call() {
+    local BINARY=$1
+    for FUNCTION in $ZSH_LAZY_NVM_BINARIES
+    do
+        unset -f $FUNCTION
+    done
+    zgen oh-my-zsh plugins/nvm
+    $@
+}
+
 for NODE_BINARY in $ZSH_LAZY_NVM_BINARIES
 do
-    $NODE_BINARY() {
-        for FUNCTION in $ZSH_LAZY_NVM_BINARIES
-        do
-            unset -f $FUNCTION
-        done
-        zgen oh-my-zsh plugins/nvm
-        $NODE_BINARY $@
-    }
+    eval "$NODE_BINARY() _zsh_lazy_nvm_replace_binaries_and_call '$NODE_BINARY'"
 done
